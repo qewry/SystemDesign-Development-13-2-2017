8d4d410de19d34169b2fd9a995fc21363e3f00a8
qewry <sboulasikis@deicollege.gr>
2017-02-13 19:08:47 +0200

    Create FirstGUI.java
    /*
     * 
     * 
     * 
     * 
     * //an ekana JFrame.DO_NOTHING_ON_CLOSE den 8a ekleine patwntas to x
     * 
     * 
     * //Ta container einai upoklassh twn component
     * 
     * 	APO EDW KATW 8A HTAN MESA SAUTH TH MAIN	
     *		 * edw exoume ftiaksei ta idia pragmata mesa sthn
     *		 * SimpleFrame
     *		 * ekei den xreiazetai na kanoume JFrame.methodos
     *		 * epeidh einai hdh JFrame afou kanei extends thn
     *		 * klassh JFrame
     *			JFrame f = new JFrame("My first GUI");
     *			JButton button = new JButton("Press me!");
     *			f.getContentPane().add(button);
     *			f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     *			f.pack();
     *			f.setVisible(true);
     *			
     *			edw apla kanoume ena object typou SimpleFrame
     *			to opoio einai hdh JFrame
     *		
     *		
     * 
     */

---
+++ FirstGUI.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+
+import javax.swing.JFrame;
+
+public class FirstGUI {
+	
+	public static void main(String[] args) {
+
+		SimpleFrame s = new SimpleFrame();
+		s.setVisible(true);
+			
+	}
+}
+
+/*
+ * 
+ * 
+ * 
+ * 
+ * //an ekana JFrame.DO_NOTHING_ON_CLOSE den 8a ekleine patwntas to x
+ * 
+ * 
+ * //Ta container einai upoklassh twn component
+ * 
+ * 	APO EDW KATW 8A HTAN MESA SAUTH TH MAIN	
+ *		 * edw exoume ftiaksei ta idia pragmata mesa sthn
+ *		 * SimpleFrame
+ *		 * ekei den xreiazetai na kanoume JFrame.methodos
+ *		 * epeidh einai hdh JFrame afou kanei extends thn
+ *		 * klassh JFrame
+ *			JFrame f = new JFrame("My first GUI");
+ *			JButton button = new JButton("Press me!");
+ *			f.getContentPane().add(button);
+ *			f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ *			f.pack();
+ *			f.setVisible(true);
+ *			
+ *			edw apla kanoume ena object typou SimpleFrame
+ *			to opoio einai hdh JFrame
+ *		
+ *		
+ * 
+ */
+
